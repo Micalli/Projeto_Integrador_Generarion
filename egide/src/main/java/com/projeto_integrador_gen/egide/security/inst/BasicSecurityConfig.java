19f69e282cfeb50023cb075ef419d275df12878f
unknown <marisantos2305@hotmail.com>
2021-04-12 10:04:27 -0300

    Atualização

--- egide/src/main/java/com/projeto_integrador_gen/egide/security/inst/BasicSecurityConfig.java
+++ egide/src/main/java/com/projeto_integrador_gen/egide/security/inst/BasicSecurityConfig.java
@@ -41,7 +41,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
-		
+	
 	}
 	
 
